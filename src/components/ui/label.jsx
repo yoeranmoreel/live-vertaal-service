cb91604bd8c217d696db85fa4b3b3c09887dc912
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 16:54:28 +0100

    Update label.jsx

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import * as React from "react"
 
-export const Label = ({ children, className = "", ...props }) => (
-  <label className={`block mb-1 font-medium ${className}`} {...props}>
-    {children}
-  </label>
-);
+const Label = React.forwardRef(({ className = "", ...props }, ref) => {
+  return (
+    <label
+      ref={ref}
+      className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className}`}
+      {...props}
+    />
+  )
+})
+
+Label.displayName = "Label"
+
+export { Label }
