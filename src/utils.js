af7824fbf054f373a1abdabddeef203b558a99e7
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 12:56:35 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
+// src/utils.js
+
+// Functie om een pagina URL te maken
+export function createPageUrl(page) {
+  return `/pages/${page}`; // pas aan als jullie routing anders is
+}
+
+// Hier kunnen later andere helper functies bij
+// export function otherHelper() { ... }
+
 
