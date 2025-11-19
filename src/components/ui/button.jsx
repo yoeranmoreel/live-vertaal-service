1136779859cb37658a84863a9f3b54ec1f772b0e
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 13:23:00 +0100

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,3 +1,8 @@
 import React from "react";
 
-export const Button = (props) => <button {...props}>Button</button>;
+export const Button = (props) => (
+  <button
+    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 disabled:opacity-50"
+    {...props}
+  />
+);
