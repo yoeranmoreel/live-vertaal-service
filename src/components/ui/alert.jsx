5bf1c499581322412f142f2a87a1fe4712981b85
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 13:06:23 +0100

    Update alert.jsx

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-export const Alert = (props) => {
-  return <div {...props}>Alert</div>;
-};
-
-
+export const Alert = (props) => <div {...props}>Alert</div>;
+export const AlertDescription = (props) => <div {...props}>AlertDescription</div>;
+export const AlertTitle = (props) => <div {...props}>AlertTitle</div>;
