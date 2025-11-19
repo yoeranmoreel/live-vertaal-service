211fb414e8df677a2eac674160eb54089a76b693
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 13:13:13 +0100

    Update dialog.jsx

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -1,3 +1,4 @@
+// src/components/ui/dialog.jsx
 import React from "react";
 
 export const Dialog = (props) => <div {...props}>Dialog</div>;
@@ -5,3 +6,4 @@ export const DialogTrigger = (props) => <div {...props}>DialogTrigger</div>;
 export const DialogContent = (props) => <div {...props}>DialogContent</div>;
 export const DialogHeader = (props) => <div {...props}>DialogHeader</div>;
 export const DialogFooter = (props) => <div {...props}>DialogFooter</div>;
+export const DialogTitle = (props) => <div {...props}>DialogTitle</div>;
