06ba9d1de2ec166278c72cb854635d32684ab0a6
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 13:07:35 +0100

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-export const Card = (props) => {
-  return <div {...props}>Card</div>;
-};
-
-
+export const Card = (props) => <div {...props}>Card</div>;
+export const CardHeader = (props) => <div {...props}>CardHeader</div>;
+export const CardContent = (props) => <div {...props}>CardContent</div>;
+export const CardFooter = (props) => <div {...props}>CardFooter</div>;
