365957699ceadaa0e600caf897e93ecf5b090668
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-19 13:17:37 +0100

    Update select.jsx

--- src/components/ui/select.jsx
+++ src/components/ui/select.jsx
@@ -1,3 +1,8 @@
+// src/components/ui/select.jsx
 import React from "react";
 
 export const Select = (props) => <select {...props}></select>;
+export const SelectTrigger = (props) => <div {...props}>SelectTrigger</div>;
+export const SelectContent = (props) => <div {...props}>SelectContent</div>;
+export const SelectItem = (props) => <div {...props}>SelectItem</div>;
+export const SelectValue = (props) => <div {...props}>SelectValue</div>;
