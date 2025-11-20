efc0b55b1230cd60a7f8f36e024c917a9a88200e
yoeranmoreel <yoeranmoreel@gmail.com>
2025-11-20 21:53:36 +0100

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+  clients.claim();
+});
+
+// Optioneel: simpele cache voor offline starten
+const CACHE_NAME = "lvs-cache-v1";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
